c7376187c1cc65ea295191d72de31c691ea6fa6e
conorpark@naver.com <conorpark@naver.com>
2021-08-24 09:31:53 +0900

    백준온라인저지 - 2798 - 블랙잭

---
+++ src/baekjoon/Bruteforce_2798.java
@@ -0,0 +1,44 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 블랙잭
+// https://www.acmicpc.net/problem/2798
+public class Bruteforce_2798 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer token = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(token.nextToken());
+		int m = Integer.parseInt(token.nextToken());
+		int[] cards = new int[n];
+		
+		token = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++)
+		{
+			cards[i] = Integer.parseInt(token.nextToken());
+		}
+		
+		int max = 0;
+		for(int x = 0; x < n; x++)
+		{
+			for(int y = x + 1; y < n; y++)
+			{
+				for(int z = y + 1; z < n; z++)
+				{
+					int sum = cards[x] + cards[y] + cards[z];
+					if(sum <= m)
+					{
+						max = Math.max(max, sum);
+					}
+				}
+			}
+		}
+		System.out.println(max);
+		
+	}
+
+}
