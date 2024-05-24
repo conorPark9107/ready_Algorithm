09041642a02015d13c1b001f0fdf5434a7549134
conorpark@naver.com <conorpark@naver.com>
2024-05-24 16:08:41 +0900

    기초 - 진법 변환 2

---
+++ src/basics/Baekjoon_11005.java
@@ -0,0 +1,38 @@
+package basics;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Baekjoon_11005 {
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] strArr = br.readLine().split(" ");
+
+        String B = strArr[0];
+        int N = Integer.parseInt(strArr[1]);
+
+        int result = 0;
+        for (int i = 0; i < B.length(); i++) {
+            char c = B.charAt(B.length() - 1 - i);
+
+            if (c > '9') {
+                c = (char) (c - 'A' + 10);
+            }else{
+                c = (char) (c - '0');
+            }
+
+            int avg = 1;
+            for(int j = 0; j < i; j++){
+                avg = avg * N;
+            }
+
+            result += avg * c;
+
+
+        }
+
+        System.out.println(result);
+    }
+
+}
