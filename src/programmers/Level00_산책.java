e4ab661a824f26f8bb67778d277cd3eb76107985
conorPark9107 <conorpark@naver.com>
2025-07-14 20:33:52 +0900

    프로그래머스 기초 다지기 (5)

---
+++ src/programmers/Level00_산책.java
@@ -0,0 +1,39 @@
+package programmers;
+
+import java.util.Arrays;
+
+// 산책
+// https://school.programmers.co.kr/learn/courses/30/lessons/250129
+public class Level00_산책 {
+
+    public static void main(String[] args) {
+        Arrays.stream(solution("NSSNEWWN")).forEach(System.out::print); //  [-1, 1]
+        System.out.println();
+        Arrays.stream(solution("EESEEWNWSNWWNS")).forEach(System.out::print); // [0, 0]
+    }
+
+    public static int[] solution(String route) {
+        int east = 0;
+        int north = 0;
+        int[] answer = new int [2];
+        for(int i=0; i<route.length(); i++){
+            switch(route.charAt(i)){
+                case 'N':
+                    north++;
+                    break;
+                case 'S':
+                    north--;
+                    break;
+                case 'E':
+                    east++;
+                    break;
+                case 'W':
+                    east--;
+                    break;
+            }
+        }
+        answer[0] = east;
+        answer[1] = north;
+        return answer;
+    }
+}
