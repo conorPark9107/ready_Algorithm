940e362b0071d0202aed5c6fca96d001ff58c37a
conorPark9107 <conorpark@naver.com>
2025-08-25 20:20:53 +0900

    프로그래머스 기초 다지기 (24)

---
+++ src/programmers/level00_9로나눈나머지.java
@@ -0,0 +1,33 @@
+package programmers;
+
+// 9로 나눈 나머지
+// https://school.programmers.co.kr/learn/courses/30/lessons/181914
+public class level00_9로나눈나머지 {
+
+    public static void main(String[] args) {
+        System.out.println(solution("123")); // 6
+        System.out.println(solution("78720646226947352489")); // 2
+    }
+
+//    public static int solution(String number) {
+//        int answer = 0;
+//
+//        for(String c : number.split("")){
+//            answer += Integer.parseInt(c);
+//        }
+//
+//
+//        return answer % 9;
+//    }
+
+    // 번외 큰 숫자를 나누기.
+    public static int solution(String number) {
+        int answer = 0;
+
+
+
+
+        return answer % 9;
+    }
+
+}
