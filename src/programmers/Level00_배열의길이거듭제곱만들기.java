1523fe1f8dfc6c042d10c7a4ac7b32c9ccd3d563
conorPark9107 <conorpark@naver.com>
2025-11-19 19:18:50 +0900

    프로그래머스 기초 다지기 (52)

---
+++ src/programmers/Level00_배열의길이거듭제곱만들기.java
@@ -0,0 +1,36 @@
+package programmers;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+// 배열의 길이를 2의 거듭제곱으로 만들기
+// https://school.programmers.co.kr/learn/courses/30/lessons/181857
+public class Level00_배열의길이거듭제곱만들기 {
+
+    public static void main(String[] args) {
+        int[] arr = solution(new int[]{1, 2, 3, 4, 5, 6});
+        Arrays.stream(arr).forEach(value -> System.out.print(value + " ")); // [1, 2, 3, 4, 5, 6, 0, 0]
+        System.out.println();
+
+        arr = solution(new int[]{58, 172, 746, 89});
+        Arrays.stream(arr).forEach(value -> System.out.print(value + " ")); // [58, 172, 746, 89]
+        System.out.println();
+
+        arr = solution(new int[]{1});
+        Arrays.stream(arr).forEach(value -> System.out.print(value + " ")); // [1, 0]
+        System.out.println();
+
+        arr = solution(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9});
+        Arrays.stream(arr).forEach(value -> System.out.print(value + " ")); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0, 0, 0, 0, 0, 0]
+        System.out.println();
+    }
+
+    public static int[] solution(int[] arr) {
+        int count = 1;
+        while((int)Math.pow(2.0, count) < arr.length) count++;
+        int[] answer = new int[(int)Math.pow(2.0, count)];
+        System.arraycopy(arr, 0, answer, 0, arr.length);
+        return answer;
+    }
+
+}
