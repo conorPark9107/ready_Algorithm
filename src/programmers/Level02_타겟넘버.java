7709840361ab6d3751a35dd9275df18cd0671c43
conorPark9107 <conorpark@naver.com>
2026-02-04 09:58:38 +0900

    프로그래머스 2 Level (타겟넘버, DFS, 멤버 변수를 제거하고 풀기.)

--- src/programmers/Level02_타겟넘버.java
+++ src/programmers/Level02_타겟넘버.java
@@ -10,24 +10,46 @@ public class Level02_타겟넘버 {
         System.out.println(o.solution(new int[]{4, 1, 2, 1}, 4)); // 2
     }
 
-    int answer = 0;
-    int[] numbers;
-    int target;
+
     public int solution(int[] numbers, int target) {
-        this.answer = 0;
-        this.numbers = numbers;
-        this.target = target;
-        dfs(0, 0);
-        return answer;
+        return dfs(numbers, 0, target, 0);
     }
 
-    private void dfs(int index, int current) {
-        if(numbers.length == index) {
-            if(target == current) answer++;
-            return;
+
+    private int dfs(int[] numbers, int index, int target, int current) {
+        int count = 0;
+        if(index == numbers.length){
+            if(current == target) {
+                count++;
+            }
+            return count;
         }
-        dfs(index + 1, current + numbers[index]);
-        dfs(index + 1, current - numbers[index]);
+        count += dfs(numbers, index + 1, target, current + numbers[index]);
+        count += dfs(numbers, index + 1, target, current - numbers[index]);
+        return count;
     }
+
+
+
+//    int answer = 0;
+//    int[] numbers;
+//    int target;
+//    public int solution(int[] numbers, int target) {
+//        this.answer = 0;
+//        this.numbers = numbers;
+//        this.target = target;
+//        dfs(0, 0);
+//        return answer;
+//    }
+//
+//
+//    private void dfs(int index, int current) {
+//        if(numbers.length == index) {
+//            if(target == current) answer++;
+//            return;
+//        }
+//        dfs(index + 1, current + numbers[index]);
+//        dfs(index + 1, current - numbers[index]);
+//    }
 
 }
