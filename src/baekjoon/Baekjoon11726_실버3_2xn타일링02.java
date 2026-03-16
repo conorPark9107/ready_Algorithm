6288e8d3b01b6ed58a176d92b3f805826b5645a9
conorPark9107 <conorpark@naver.com>
2026-03-16 09:48:36 +0900

    Baekjoon 11726, 2*n타일링 , 실버 3 (DP 기초복습)

---
+++ src/baekjoon/Baekjoon11726_실버3_2xn타일링02.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 2 * n 타일링
+// https://www.acmicpc.net/problem/11726
+public class Baekjoon11726_실버3_2xn타일링02 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int a = 1;
+        int b = (n == 1) ? 1 : 2;
+        for (int i = 3; i <= n; i++) {
+            int c = (a + b) % 10007;
+            a = b;
+            b = c;
+        }
+        System.out.println(b);
+    }
+}
