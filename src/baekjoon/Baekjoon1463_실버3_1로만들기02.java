6d5c2c5a497c37e8350cb227e1665ec81dcb9918
conorPark9107 <conorpark@naver.com>
2026-03-16 09:48:55 +0900

    Baekjoon 1463, 1로만들기 , 실버 3 (DP 기초복습)

---
+++ src/baekjoon/Baekjoon1463_실버3_1로만들기02.java
@@ -0,0 +1,28 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 1로 만들기
+// https://www.acmicpc.net/problem/1463
+public class Baekjoon1463_실버3_1로만들기02 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 1];
+        dp[1] = 0;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + 1;
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i] = dp[i / 3] + 1);
+            }
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i] = dp[i / 2] + 1);
+            }
+        }
+        System.out.println(dp[n]);
+    }
+
+}
