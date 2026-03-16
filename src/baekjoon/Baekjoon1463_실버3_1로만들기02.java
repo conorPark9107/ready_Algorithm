7f6a264785d7a2a3f464a63b034ac5b49e2384d5
conorPark9107 <conorpark@naver.com>
2026-03-16 09:50:53 +0900

    Baekjoon 1463, 1로만들기 , 실버 3 (DP 기초복습)

--- src/baekjoon/Baekjoon1463_실버3_1로만들기02.java
+++ src/baekjoon/Baekjoon1463_실버3_1로만들기02.java
@@ -16,10 +16,10 @@ public class Baekjoon1463_실버3_1로만들기02 {
         for (int i = 2; i <= n; i++) {
             dp[i] = dp[i - 1] + 1;
             if (i % 3 == 0) {
-                dp[i] = Math.min(dp[i], dp[i] = dp[i / 3] + 1);
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
             }
             if (i % 2 == 0) {
-                dp[i] = Math.min(dp[i], dp[i] = dp[i / 2] + 1);
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
             }
         }
         System.out.println(dp[n]);
