dfae65778bcc6abf40421b0e9909de0406461e05
conorPark9107 <conorpark@naver.com>
2026-03-26 11:01:07 +0900

    https://www.acmicpc.net/problem/2293, 골드 4 , (DP, 동전 1)

---
+++ src/baekjoon/Baekjoon2293_골드4_동전1.java
@@ -0,0 +1,33 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 동전 1
+// https://www.acmicpc.net/problem/2293
+public class Baekjoon2293_골드4_동전1 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[k + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int j = coin; j <= k; j++) {
+                dp[j] += dp[j - coin];
+            }
+        }
+
+        System.out.println(dp[k]);
+    }
+
+}
