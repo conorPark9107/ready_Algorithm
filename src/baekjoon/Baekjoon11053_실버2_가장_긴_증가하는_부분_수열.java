31cf71adbe808d72915b45e5b07abf9f1fd7ef7a
conorPark9107 <conorpark@naver.com>
2026-03-26 12:00:22 +0900

    https://www.acmicpc.net/problem/11053, 실버 2 , (DP, 가장 긴 증가하는 부분 수열)

---
+++ src/baekjoon/Baekjoon11053_실버2_가장_긴_증가하는_부분_수열.java
@@ -0,0 +1,36 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// 가장 긴 증가하는 부분 수열
+// https://www.acmicpc.net/problem/11053
+public class Baekjoon11053_실버2_가장_긴_증가하는_부분_수열 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        System.out.println(Arrays.stream(dp).max().getAsInt());
+    }
+
+}
