872a879f856424f20e6d259cbd4e0bd45e90e141
conorPark9107 <conorpark@naver.com>
2026-04-03 10:28:19 +0900

    https://www.acmicpc.net/problem/14520, 골드 3, (구현, bfs(상태관리), 벽 부수고 이동하기)

---
+++ src/baekjoon/Baekjoon2206_골드3_벽_부수고_이동하기.java
@@ -0,0 +1,78 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
+
+// 벽 부수고 이동하기
+// https://www.acmicpc.net/problem/2206
+public class Baekjoon2206_골드3_벽_부수고_이동하기 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] map = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            char[] c = br.readLine().toCharArray();
+            for (int j = 0; j < m; j++) {
+                map[i][j] = c[j] - '0';
+            }
+        }
+
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+
+        boolean[][][] visited = new boolean[n][m][2];
+        ArrayDeque<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{0, 0, 0, 1}); // x, y, 벽을 뚫었는지, 거리
+        visited[0][0][0] = true;
+
+        while (!q.isEmpty()) {
+            int[] curr = q.poll();
+            int x = curr[0];
+            int y = curr[1];
+            int b = curr[2];
+            int d = curr[3];
+
+            if (x == n - 1 && y == m - 1) {
+                System.out.println(d);
+                return;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+
+                if (map[nx][ny] == 0) {
+                    if (!visited[nx][ny][b]) {
+                        visited[nx][ny][b] = true;
+                        q.offer(new int[]{nx, ny, b, d + 1});
+                    }
+                } else if (map[nx][ny] == 1 && b == 0) {
+                    if (!visited[nx][ny][1]) {
+                        visited[nx][ny][1] = true;
+                        q.offer(new int[]{nx, ny, 1, d + 1});
+                    }
+                }
+            }
+        }
+        System.out.println(-1);
+    }
+
+    public static void print(int[][] map) {
+        System.out.println("-----------------------");
+        for (int[] ints : map) {
+            for (int anInt : ints) {
+                System.out.print(anInt + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
+
